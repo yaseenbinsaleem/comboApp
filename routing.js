4382242216e98699ed006ebf97578cf74d3a8f83
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2022-10-31 17:37:43 +0500

    Add files via upload

--- routing.js
+++ routing.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SignIn from "./signIn";
 import SignUp from "./signUp";
 import Home from "./home";
+import { AuthContextProvider } from "./authContext";
 
 export default function Routing() {
   return (
-<Router>
-
-    <Routes>
+<AuthContextProvider>
+<Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signIn" element={<SignIn />} />
       <Route path="/signUp" element={<SignUp />} />
     </Routes>
-</Router>
+</AuthContextProvider>
+   
+
   );
 }
