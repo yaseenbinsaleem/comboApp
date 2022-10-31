4b31c92bb2db4a2f3035e3bae2b0829d1521acb1
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2022-10-31 17:37:57 +0500

    Add files via upload

--- signIn.js
+++ signIn.js
@@ -1,51 +1,50 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-    
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { auth, signInWithEmailAndPassword } from "./firebase";
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useNavigate} from "react-router-dom";
 
 const theme = createTheme();
 
 export default function SignIn() {
-    const LogInData = async () => {
-        await signInWithEmailAndPassword(
-           auth,
-           registrationData.email,
-           registrationData.password
-         )
-           .then((userCredential) => {
-             const user = userCredential.user;
-             console.log("done");
-           })
-           .catch((error) => {
-             const errorCode = error.code;
-             const errorMessage = error.message;
-             // ..
-           });
-       };
-       const [registrationData, setRegistrationData] = useState({
-        email: "",
-        password: ""
-      });
-    
-      const Registration = (e) => {
-        setRegistrationData({
-          ...registrationData,
-          [e.target.name]: e.target.value
-        });
-      };
+  const [registrationData, setRegistrationData] = useState({
+    email: "",
+    password: ""
+  });
+  const navigate = useNavigate()
+
+  const Registration = (e) => {
+    setRegistrationData({
+      ...registrationData,
+      [e.target.name]: e.target.value
+    });
+  };
+  const LoginData = async (e) => {
+    e.preventDefault()
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        registrationData.email,
+        registrationData.password
+      );
+      navigate("/")
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
@@ -53,24 +52,25 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={LogInData} noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={LoginData} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
               fullWidth
               label="Email Address"
               name="email"
+              // uper registration mai name ko mangwaenge or iske against onchange wali value add kardenge is tarha handle karte hai same state or function se data
               autoComplete="email"
               autoFocus
               onChange={Registration}
@@ -82,15 +82,13 @@ export default function SignIn() {
               name="password"
               label="Password"
               type="password"
-            
               autoComplete="current-password"
               onChange={Registration}
-
             />
-            <FormControlLabel
+            {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
-            />
+            /> */}
             <Button
               type="submit"
               fullWidth
@@ -100,13 +98,13 @@ export default function SignIn() {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
+              {/* <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
                 </Link>
-              </Grid>
+              </Grid> */}
               <Grid item>
-              Don't have an account? 
+                Don't have an account? 
                 <Link to="/signUp" variant="body2">
                   Sign Up
                 </Link>
@@ -114,7 +112,6 @@ export default function SignIn() {
             </Grid>
           </Box>
         </Box>
-        
       </Container>
     </ThemeProvider>
   );
